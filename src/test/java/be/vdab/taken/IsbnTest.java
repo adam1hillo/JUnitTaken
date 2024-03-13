8cff064979aaa4989f9f80db2a1025e81a53f8ee
Adam Hillo <adam1hillo@gmail.com>
2024-03-13 14:47:00 +0100

    Parameterized tests

--- src/test/java/be/vdab/taken/IsbnTest.java
+++ src/test/java/be/vdab/taken/IsbnTest.java
@@ -2,6 +2,8 @@ package be.vdab.taken;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -9,43 +11,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class IsbnTest {
 
-    @Test
-    void hetNummer0IsVerkeerd() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Isbn(0));
-    }
-    @Test
-    void eenNegatiefNummerIsVerkeerd() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Isbn(-9789027439642L));
+    @ParameterizedTest
+    @ValueSource(longs = {0, -9789027439642L, 978902743964L, 97890274396421L, 9779227439643L,
+            8789027439642L, 9789027439643L})
+    void verkeerdeNummers(long nummer) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Isbn(nummer));
     }
 
-    @Test
-    void eenNummerMet14CijfersIsVerkeerd() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Isbn(97890274396421L));
-    }
-    @Test
-    void eenNummerMet12CijfersIsVerkeerd() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Isbn(978902743964L));
-    }
-    @Test
-    void deEerste3CijfersMoeten978of979Zijn() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Isbn(9189027439642L));
-    }
-    @Test
-    void eenNummerMet13CijfersMetVerkeerdControleGetal2() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Isbn(9789027419542L));
-    }
-
-    @Test
-    void eenNummerMet13CijfersMetCorrectControleGetal2() {
-        new Isbn(9789027439642L);
-    }
-    @Test
-    void eenNummerMet13CijfersMetVerkeerdControleGetal3() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Isbn(9789027439643L));
-    }
-    @Test
-    void eenNummerMet13CijfersMetCorrectControleGetal0() {
-        new Isbn(9789227439640L);
+    @ParameterizedTest
+    @ValueSource(longs = {9789027439642L, 9789227439640L})
+    void correcteNummers(long nummer) {
+        new Isbn(nummer);
     }
     @Test
     void toStringGeeftNummerTerug() {
